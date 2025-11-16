f814367a3952a7b540c26b003e52029fec249031
GooseJacket <73757413+GooseJacket@users.noreply.github.com>
2025-11-16 12:25:21 -0800

    Upload New Text Classification

--- textclass.py
+++ textclass.py
@@ -1,6 +1,6 @@
 import torch
-from oldsource.model import train, evaluate, NeuralNetwork
-from oldsource.utils import load_data, TextProcessor, convert_text_to_tensors
+from source.model import train, evaluate, NeuralNetwork
+from source.utils import load_data, TextProcessor, convert_text_to_tensors
 
 if __name__ == "__main__":
 
@@ -12,15 +12,19 @@ if __name__ == "__main__":
     ####################
 
     # Load training and test olddata
-    train_texts, train_labels = load_data("../olddata/total_train.txt")
-    test_texts, test_labels = load_data("../olddata/total_test.txt")
+    train_texts, train_labels = load_data("../data/train.txt")
+    test_texts, test_labels = load_data("../data/test.txt")
+
+    # DEBUGGING DATA (smaller to speed up training and ensure no dumb mistakes)
+    # train_texts, train_labels = train_texts[0:1000], train_labels[0:1000]
+    # test_texts, test_labels = test_texts[0:1000], test_labels[0:1000]
 
     # Preprocess text
     processor = TextProcessor(vocab_size=10000)
     processor.build_vocab(train_texts)
 
     # Convert text documents to tensor representations of word indices
-    max_length = 1250
+    max_length = 400
     train_features = convert_text_to_tensors(train_texts, processor, max_length)
     test_features = convert_text_to_tensors(test_texts, processor, max_length)
 
@@ -29,9 +33,9 @@ if __name__ == "__main__":
     vocab_size = len(processor.word_to_idx)
     embedding_dim = max_length
     hidden_size = 128
-    output_size = 2  # Binary classification for sentiment analysis
+    output_size = 4
 
-    print("Variables:")
+    print("\nVariables:")
     print("\tTraining size\t", len(train_texts), "texts x", max_length, "tokens")
     print("\tTest size\t\t", len(test_texts), "texts x", max_length, "tokens")
     print("\tForward pass\t", embedding_dim, "embed ->", hidden_size, "hidden ->", output_size, "classes")
@@ -49,6 +53,8 @@ if __name__ == "__main__":
     except:
         ne = 25
     finally:
+        if ne == 0:
+            ne = 1
         print("Okay, ", ne, "Epochs.")
 
     # Train
@@ -62,12 +68,11 @@ if __name__ == "__main__":
     print(f"Test accuracy: {evaluation_results['test_accuracy']:.4f}")
     print(f"Test F1 score: {evaluation_results['test_f1']:.4f}")
 
-    print("\nMistaken Tests:")
-    for i in evaluation_results['error_indexes']:
-        print("\t- ", test_texts[i])
+    print("\nGuesses in Each Category: (real average =", len(test_texts)/output_size, ")")
+    print("Class # \tTrue\tFalse")
+    for i in range(4):
+        print("Class" + str(i), evaluation_results['guesses'][i][0], evaluation_results['guesses'][i][1], sep="\t")
 
     # Save model weights to file
     torch.save(model.state_dict(), outfile)
     print(f"Trained model saved to {outfile}")
-
-
