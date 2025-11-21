5e203a91b4bc882ae453e42092e464d41daa53bc
GooseJacket <73757413+GooseJacket@users.noreply.github.com>
2025-11-20 16:19:53 -0800

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -4,7 +4,7 @@ from random import shuffle
 
 
 def clean_string(text, remove):
-    remove.append(r'[^a-zA-Z\s]')
+    text = re.sub(r'[^a-zA-Z\s(\[PB\])(\[NL\])]', "", text)
     remove.append("\n")
 
     for r in remove:
@@ -13,28 +13,29 @@ def clean_string(text, remove):
     return text
 
 
-def strip_book(file, label, squeezeNum):  # returns book file as list of paragraph-label pairs (tests)
+def strip_book(file, label, squeezeNum, total):  # returns book file as list of paragraph-label pairs (tests)
     book = ""
     with open(file, 'r', encoding='utf-8') as F:
         for line in F:
-            book += line
+            book += line + " "
 
     # Fix the line/paragraph breaks:
-    book = re.sub("\n\n", "PARABREAK", book)
+    book = re.sub("\n\n", " [PB] ", book)
     if label == 1 or label == 3: #poetry and shakespeare care about line breaks
         book = re.sub("\n", " [NL] ", book)
     else:
         book = re.sub("\n", "", book)
-    book = book.split("PARABREAK")
-    ret = squeeze(book, squeezeNum, label)
+    # book = book.split("PARABREAK")
+    ret = squeeze(book, squeezeNum, label, total=total)
+
 
     return ret
 
 
-def squeeze(book, num, label):
+def squeeze(book, squeezeNum, label, num_tokens=100, total=7000):
     ret = []
 
-    if num == 1:
+    '''if num == 1:
         ret = [ [i, label] for i in book]
         return ret
 
@@ -46,7 +47,25 @@ def squeeze(book, num, label):
 
     spare = len(book) % num
     if spare > 0:
-        ret.extend(squeeze(book[len(book)-spare:], num-1, label))
+        ret.extend(squeeze(book[len(book)-spare:], num-1, label))'''
+
+    book = book.split(" ")
+    book = [i for i in book if i != ""]
+
+    stops = ["[NL]", ".", "!", "?", "--", "[PB]"]
+
+    i = 0
+    while i < min(int((total/squeezeNum)*num_tokens), len(book)):
+        start = i
+        fullstop = min(i+num_tokens, len(book))
+
+        i = start + int((fullstop - start) / 2)
+
+        while i < fullstop and book[i] not in stops:
+            i += 1
+        ret.append([clean_string(" ".join(book[start:i]), []), label])
+        #print(ret[-1])
+        i += 1
 
     return ret
 
@@ -58,22 +77,27 @@ for t in test:
     '''
 
 
-def load_runs():
+def load_runs(total_num):
     train_test_valids = [ [], [], [] ]
 
     paths = ["Movies", "Poetry", "Romance", "Shakespeare"]
-    squNum = [3, 4, 5, 2]
+    squNum = [15, 20, 15, 1]
     for i in range(len(paths)):
         set = []
         files = os.listdir("../data/ClassicBooks/" + paths[i])
 
         for file in files:
-            set.extend(strip_book("../data/ClassicBooks/" + paths[i] + "/" + file, i, squNum[i]))
-            if(len(set) >= 5000):
+            #def squeeze(book, squeezeNum, label, num_tokens=100, total=7000):
+            #set.extend(strip_book("../data/ClassicBooks/" + paths[i] + "/" + file, squNum[i], i, total=total_num))
+            set.extend(strip_book("../data/ClassicBooks/" + paths[i] + "/" + file, squeezeNum=squNum[i], label=i, total=total_num))
+
+            if(len(set) >= total_num):
                 break
 
         print(paths[i], len(set))
         print("\t", len(set) * .75, len(set) * .20)
+
+        shuffle(set)
 
         # dump this book type into the real lists
         train_test_valids[0].extend(set[0: int(len(set) * 0.75)])
@@ -114,6 +138,8 @@ def load_poems():
 
 
 # load_poems()
-load_runs()
+load_runs(3000)
+
+# strip_book("../data/ClassicBooks/Shakespeare/as you like it.txt", 1, 1)
 
 print("All done!")
